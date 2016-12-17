583981caa76f0e2019dee7b16665e4a0c5083372
mibo <mibo@mirb.de>
2016-12-17 07:56:46 +0100

    Tests with trailing '/' and uri info

--- src/main/java/de/mirb/pg/rest/endpoint/BeanEndpoint.java
+++ src/main/java/de/mirb/pg/rest/endpoint/BeanEndpoint.java
@@ -5,8 +5,10 @@ import de.mirb.pg.rest.data.Bean;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,12 +34,42 @@ public class BeanEndpoint {
   }
 
   @GET
+  @Path("single")
+  @Produces({ MediaType.APPLICATION_XML })
+  public Bean singleBeanWithAcceptXml() {
+
+    return new Bean("hey", "XML bean!");
+  }
+
+  @GET
   @Path("multi")
   @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM })
-  public List<Bean> multiBean() {
-
-    return createBeans(10);
+  public List<Bean> multiBean(@Context UriInfo uriInfo) {
+    if(uriInfo.getPath().endsWith("/")) {
+      return createBeans(10);
+    }
+    return createBeans(1);
   }
+
+  /*
+   * XXX: This do not work:
+   *
+   * [[FATAL] A resource model has ambiguous (sub-)resource method for HTTP method GET and
+   * input mime-types as defined by"@Consumes" and "@Produces" annotations at Java methods
+   * public java.util.List de.mirb.pg.rest.endpoint.BeanEndpoint.multiBean(javax.ws.rs.core.UriInfo)
+   * and public java.util.List de.mirb.pg.rest.endpoint.BeanEndpoint.multiBeanSlash()
+   * at matching regular expression /multi.
+   * These two methods produces and consumes exactly the same mime-types and therefore their invocation
+   * as a resource methods will always fail.; source='org.glassfish.jersey.server.model.RuntimeResource@709c903f'].
+   * Please see server.log for more details.
+   */
+//  @GET
+//  @Path("multi/")
+//  @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM })
+//  public List<Bean> multiBeanSlash() {
+//    return createBeans(10);
+//  }
+
 
   @GET
   @Path("response/multi")
